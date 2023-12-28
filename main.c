76af853212df47739b4b0f0f4cc8c0b79a503352
Eduardo Yoshikawa <eduardo.yoshikawa@outlook.com>
2023-12-28 18:23:30 -0300

    2o commit: a - Foram acrescentados vetores localidade e estado. b - Feito uso da estrutura de repetição for para viabilizar a entrada de dados em cada elemento dos vetores.

--- main.c
+++ main.c
@@ -16,11 +16,13 @@ int main()
         {
             printf("O numero de alunos nao pode ser maior do que zero!!!\n");
         }
-        else if (numeroAlunos >10){
+        else if (numeroAlunos >10)
+        {
             printf("A quantidade maxima de alunos eh de 10 alunos!!!\n");
         }
 
-    }while(numeroAlunos <= 0 || numeroAlunos > 10);
+    }
+    while(numeroAlunos <= 0 || numeroAlunos > 10);
 
 
     //Declarar vetores relacionados a cada campo do cadastro do aluno
@@ -28,7 +30,35 @@ int main()
     int idade [numeroAlunos];
     char genero [numeroAlunos];
     char localidade[numeroAlunos][50];
+    char estado [numeroAlunos][2];
     char sexo [numeroAlunos];
+
+    //Fazer uso da estrutura de repetição for para dar entrada de dados, em cada elemento do vetor
+    for(iterador = 1; iterador <= numeroAlunos; iterador++)
+    {
+        printf("Digite o nome do %io aluno: ", iterador);
+        fseek(stdin,0,SEEK_END);//LIMPEZA DE BUFFER
+        gets(nome[iterador]);
+
+        printf("Informe a idade: ");
+        scanf("%d", &idade[iterador]);
+
+        printf("Informe o genero (M/F/O): ");
+        fseek(stdin,0,SEEK_END);//LIMPEZA DE BUFFER
+        scanf("%c", &genero[numeroAlunos]);
+
+        printf("Informe a cidade que o aluno mora:");
+        fseek(stdin,0,SEEK_END);//LIMPEZA DE BUFFER
+        gets(localidade[iterador]);
+
+        printf("Informe o estado: ");
+        fseek(stdin,0,SEEK_END);//LIMPEZA DE BUFFER
+        gets(estado[iterador]);
+
+        printf("Informe o curso que o aluno esta matriculado: ");
+        fseek(stdin,0,SEEK_END);//LIMPEZA DE BUFFER
+
+    }
 
 
 
